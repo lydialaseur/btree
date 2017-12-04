6421b32f7fd69279be9fb8b0e6d6b27b633f6b86
lydialaseur <lydia.laseur@ncf.edu>
2017-12-03 22:21:25 -0500

    added code to print first 2 levels of the btree

--- createMyIndex.py
+++ createMyIndex.py
@@ -11,3 +11,36 @@ p = 3
 
 idx = BTreeIndex('stories',col,3)
 num_insertions, num_levels = idx.create()
+
+
+# print the first first 2 levels of the tree
+print('Number of levels: {}'.format(num_levels))
+print('-------------------------------ROOT--------------------------------')
+# print(self.root)
+print('Keys in root: {0}'.format(idx.root.keys))
+# print('Children of root: {0}'.format(self.root.children))
+# print('Parent of root: {0}'.format(self.root.parent))
+print('\n\n')
+for l in range(num_levels+1):
+    if l == 1:
+        print('------------------------------LEVEL 1------------------------------')
+        for c in range(1,len(idx.root.children)+1):
+            print('------------------------------CHILD {0}------------------------------'.format(c))
+            print('Keys in child: {0}'.format(idx.root.children[c-1].keys))
+            # print('Children of child: {0}'.format(idx.root.children[c-1].children))
+            # print('Parent of child: {0}'.format(idx.root.children[c-1].parent))
+            print('\n')
+        print('\n')
+    if l == 2:
+        print('------------------------------LEVEL 2------------------------------')
+        for c in range(1,len(idx.root.children)+1):
+            gc = 1
+            for grandchild in idx.root.children[c-1].children:
+                print('----------------------GRANDCHILD {0} OF CHILD {1}----------------------'.format(gc,c))
+                print('Keys in grandchild: {0}'.format(grandchild.keys))
+                print('\n')
+                # print('Children of child: {0}'.format(grandchild.children))
+                # print('Parent of child: {0}'.format(grandchild.parent))
+                # print(grandchild)
+                gc += 1
+        print('\n\n')
